41a094eece5c6b904900a16e9336ecdd569a62d3
sydneyhauke <sydney.hauke@epfl.ch>
2015-06-10 21:21:30 +0200

    Classe principale TangibleGame

---
+++ cs211/TangibleGame.java
@@ -0,0 +1,10 @@
+import processing.core.PApplet;
+
+/**
+ * Created by sydney on 10.06.15.
+ */
+public class TangibleGame {
+    public static void main(String[] args) {
+        PApplet.main("--present", new String[] {Main.class.getName()});
+    }
+}
